36178d48691a8906cb2139d6ab9af9c8df901669
MagicRom06 <romainhenry60@gmail.com>
2021-09-02 23:29:20 +0200

    homepage

---
+++ pages/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
